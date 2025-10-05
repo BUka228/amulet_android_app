b0cf99b32af8c9553b6f57d01698a158081e756f
BUka228 <128601089+BUka228@users.noreply.github.com>
2025-10-05 14:29:19 +0300

    Opt in to ExperimentalKotlinGradlePluginApi in AmuletSharedMultiplatformPlugin

--- build-logic/src/main/kotlin/amulet/kmp/AmuletSharedMultiplatformPlugin.kt
+++ build-logic/src/main/kotlin/amulet/kmp/AmuletSharedMultiplatformPlugin.kt
@@ -5,11 +5,13 @@ import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 class AmuletSharedMultiplatformPlugin : Plugin<Project> {
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
     override fun apply(target: Project) = with(target) {
         pluginManager.apply("org.jetbrains.kotlin.multiplatform")
         pluginManager.apply("com.android.library")
